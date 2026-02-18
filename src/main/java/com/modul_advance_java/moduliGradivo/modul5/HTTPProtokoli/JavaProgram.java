9f1a304d44901ab61f72d5add50cfeb6bb1f9409
Milica599 <mirkovicmilica@03gmail.com>
2026-02-18 14:57:06 +0100

    HTTP PROTOKOLE PONOVI IZ MODULA NE PISES IH U PRAKSI RUCNO
    Signed-off-by: Milica599 <mirkovicmilica@03gmail.com>

---
+++ src/main/java/com/modul_advance_java/moduliGradivo/modul5/HTTPProtokoli/JavaProgram.java
@@ -0,0 +1,9 @@
+package com.modul_advance_java.moduliGradivo.modul5.HTTPProtokoli;
+
+public class JavaProgram {
+    static void main(String[] args) {
+        //nauci teoriju ovo neces pisati rucno
+        //framework poput spring-a ce pisati http protokole ali treba da znas kako ovo radi
+        //ponovi u modulima
+    }
+}
